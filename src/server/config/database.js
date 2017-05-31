d1440c72fd7141a3b32e936f18c219721acd0a5a
LNWPOR <tclnwpor@gmail.com>
2017-05-31 16:47:37 +0700

    change localhost to 127.0.0.1

--- src/server/config/database.js
+++ src/server/config/database.js
@@ -1,3 +1,3 @@
 export default {
-	'url' : 'mongodb://localhost:27017/lnwtodoapp' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://127.0.0.1:27017/lnwtodoapp' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 };
